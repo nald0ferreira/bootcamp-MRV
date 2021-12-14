307f4cbde79d9913634562e3c081c543bdd55444
naldo <ednaldocpv@gmail.com>
2021-12-14 09:49:44 -0300

    aula pratica ex2

--- introducao-javaScript/exercicios/funcoes-atividade-pratica.js
+++ introducao-javaScript/exercicios/funcoes-atividade-pratica.js
@@ -1,3 +1,5 @@
+//Alunos aprovados
+/*
 const alunos = [
     {nome: 'Ana', nota: 7},
     {nome: 'João', nota: 3},
@@ -16,3 +18,18 @@ function alunosAprovados(arr, media){
         }return aprovados;
     }
 console.log(alunosAprovados(alunos, 5));
+*/
+
+// Calcula idade
+
+
+
+function calculaIdade(anos){
+    return `Daqui a ${anos} anos, ${this.nome} terá ${
+        this.idade + anos
+    } anos de idade. `;
+}
+
+const pessoa = { nome: 'Naldo', idade: 38 };
+
+console.log(calculaIdade.call(pessoa, 5));
